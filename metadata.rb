2b0a3980bcbb88cdfc873040af0507ee8a9aeb59
Pablo Ramón <pabloramon21@gmail.com>
2014-11-02 20:51:46 -0500

    Adding name attribute

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ license          'MIT'
 description      'installs Oracle-XE version for Ubuntu'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.9'
+name             'oracle-xe'
 
 %w{ubuntu debian}.each do |os|
   supports os
